db33ed74e27d9cbc4b8ba15cf7c3dc20bc972def
Alex Crichton <alex@alexcrichton.com>
2011-04-21 11:27:50 -0400

    Add a script to run in parallel on all CMU machines

---
+++ scripts/open.rb
@@ -0,0 +1,48 @@
+require 'socket'
+require 'timeout'
+require 'net/ssh'
+require 'highline/import'
+
+hosts = []
+time  = 7
+
+['unix%02d.andrew.cmu.edu', 'ghc%02d.ghc.andrew.cmu.edu'].each do |pat|
+  (0..99).each{ |i|
+    host = pat % i
+    puts "Trying: #{host}"
+    begin
+      Timeout::timeout(0.5) { TCPSocket.new(host, 22).close }
+      hosts << host
+    rescue
+    end
+  }
+end
+
+puts "Available hosts: #{hosts.inspect}"
+
+user     = ask('User: ')
+password = ask('Password: ') { |q| q.echo = '*' }
+
+max = File.readlines('movies.lst').size + 10
+size = max / hosts.size
+
+threads = []
+
+hosts.each_with_index do |host, i|
+  start = i * size
+  last  = start + size - 1
+
+  threads << Thread.start {
+    Net::SSH.start(host, user, :password => password) do |ssh|
+      (start..last).each { |index|
+        printf "%s: %02.f%% done\n", host, ((index - start).to_f / size) * 100
+        cmd = "cd movies && nice ./find #{index} #{time}"
+        ssh.exec!(cmd) do |channel, stream, data|
+          puts "#{host}: #{stream} => #{data}"
+        end
+      }
+    end
+  }
+end
+
+threads.each(&:join)
